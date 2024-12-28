107bfd376b7493e9bb55ea8fb993a772848685a4
Charity Rymbai <charityrymbai@hotmail.com>
2024-12-28 14:36:04 +0530

    Updates

---
+++ images.js
@@ -0,0 +1,10 @@
+import profile from './profilePhoto.jpg';
+import hackerrank from './hackerrank.png';
+import iitb from './iitb.png';
+import isro from './isro_logo.png';
+import schoolLogo from './school_logo.png';
+import NITMLogo from './NITM Logo.jpeg';
+import stEdmundsLogo from './st edmunds school logo.png';
+import camlLogo from './CamlLogo.svg'
+
+export { profile, hackerrank, iitb, isro, schoolLogo, NITMLogo, stEdmundsLogo, camlLogo};
