752b78e563b3e50bb76e5d2dfede4a2c1eac9fc7
Charity Rymbai <charityrymbai@hotmail.com>
2024-12-28 14:48:36 +0530

    changed navbar from tranparent to white

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,7 +3,7 @@ import NaviButton from "./NaviButton";
 const NavigationBar = () => {
     return (
         <div className="p-4 fixed w-full flex justify-center">
-            <div className=" border-2 border-gray-400 p-4 rounded-2xl w-fit flex gap-9">
+            <div className="bg-white border-2 border-gray-400 p-4 rounded-2xl w-fit flex gap-9">
                 <NaviButton goTo="portfolio/home" title="Home" />
                 <NaviButton goTo="portfolio/about" title="About" />
                 <NaviButton goTo="portfolio/projects" title="Projects" />
